4a5332ba214687be22a40f748bf3dd5dee71d0dd
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-12-11 04:35:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,10 +88,10 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kingvj01')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Solo_leveling_Anime_Hub')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Solo_Leveling_Botz')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Solo_Leveling_Support') # Support Chat Link Without https:// or @
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/SL_Necromancer')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
