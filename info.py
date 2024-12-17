023e642e4218bd92ee4ed02653e76033c64e1a48
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-12-17 20:59:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movieboxbot1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/moviesvisit')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'movieboxbot1') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/SL_Necromancer')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Rohit1211p')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
