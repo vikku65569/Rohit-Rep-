b2e1ac5b4620f80ecccccee1258b5a874c68b63a
hello-india-SL <itz.shubham.47@gmail.com>
2024-12-20 14:34:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,10 +88,10 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movieboxbot1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/moviesvisit')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'movieboxbot1') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Rohit1211p')
+GRP_LNK = environ.get('GRP_LNK', url='https://t.me/movieboxbot1')
+CHNL_LNK = environ.get('CHNL_LNK', url='https://t.me/moviesvisit')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', url='movieboxbot1') # Support Chat Link Without https:// or @
+OWNER_LNK = environ.get('OWNER_LNK', url='https://t.me/Rohit1211p')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
