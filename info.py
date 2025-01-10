bc12eabbbf817c8f90751d9a757f3a7a2ca1d01d
vikku65569 <vikku6556956@gmail.com>
2025-01-10 20:55:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/AARLINK')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/AARLINK')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'moviesadda098765') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'movieadda0980') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/wrrohit02')
 
 # True Or False
